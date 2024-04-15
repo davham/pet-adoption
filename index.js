2b1996b9d6d86ab97462b8ab9f59201442b4fb11
David Hamilton <david.hamilton5@gmail.com>
2024-04-15 17:10:06 -0400

    working on pet cards

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const template = document.querySelector('#pet-card-template')
+const wrapper = document.createDocumentFragment()
+
 async function start() {
 	const weatherPromise = await fetch(
 		'https://api.weather.gov/gridpoints/MFL/110,50/forecast'
@@ -12,10 +15,14 @@ async function petsArea() {
 	const petsPromise = await fetch(
 		'https://learnwebcode.github.io/bootcamp-pet-data/pets.json'
 	)
-  const petsData = await petsPromise.json()
-  petsData.forEach((pet) =>{
-    console.log(pet.name)
-  })
+	const petsData = await petsPromise.json()
+	petsData.forEach((pet) => {
+		const clone = template.content.cloneNode(true)
+		clone.querySelector('h3').textContent = pet.name
+		clone.querySelector('.pet-desc').textContent = pet.description
+		wrapper.appendChild(clone)
+	})
+	document.querySelector('.list-of-pets').appendChild(wrapper)
 }
 
 petsArea()
