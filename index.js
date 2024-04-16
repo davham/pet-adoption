4ad68b410c2fd8f4591b1cf2ed7d8a724a48a6ea
David Hamilton <david.hamilton5@gmail.com>
2024-04-16 16:46:36 -0400

    Added pet default image

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ async function petsArea() {
 		clone.querySelector('h3').textContent = pet.name
 		clone.querySelector('.pet-desc').textContent = pet.description
 		clone.querySelector('.pet-age').textContent = createAgeText(pet.birthYear)
+		if (!pet.photo) pet.photo = 'images/fallback.jpg '
 		clone.querySelector('.pet-card-photo img').src = pet.photo
 		clone.querySelector(
 			'.pet-card-photo img'
